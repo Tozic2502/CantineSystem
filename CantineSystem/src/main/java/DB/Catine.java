e91026eb47cbd2f66556f9bbb8e99d114e83ca74
Mikkel <Mikcollatz@gmail.com>
2025-04-24 09:44:34 +0200

    DB con

---
+++ CantineSystem/src/main/java/DB/Catine.java
@@ -0,0 +1,47 @@
+package DB;
+
+public class Catine {
+    private int no;
+    private String name;
+    private double price;
+
+
+    public Catine(int no, String name, double price){
+        this.no=no;
+        this.name=name;
+        this.price=price;
+    }
+    public Catine(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString(){
+        return this.no + " " + this.name + " " + this.price;
+    }
+    public int getNo() {
+        return no;
+    }
+    public void setNo(int no) {
+        this.no = no;
+
+    }
+
+
+}
---
+++ CantineSystem/src/main/java/DB/CatineDAO.java
@@ -0,0 +1,11 @@
+package DB;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CatineDAO {
+
+
+
+
+}
